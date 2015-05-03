00a38035bfb41f503f9bb3b0b281938f06d41304
doroncy <doron.cy@gmail.com>
2015-05-03 23:54:40 +0300

    remove comments

--- client/app/app.js
+++ client/app/app.js
@@ -75,14 +75,3 @@ angular.module('oriApp', [
             }
         };
     });
-
-//.run(function ($rootScope, $location, Auth) {
-//  // Redirect to login if route requires auth and you're not logged in
-//  $rootScope.$on('$stateChangeStart', function (event, next) {
-//    Auth.isLoggedInAsync(function(loggedIn) {
-//      if (next.authenticate && !loggedIn) {
-//        $location.path('/login');
-//      }
-//    });
-//  });
-//});
